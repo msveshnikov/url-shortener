863660667d738c8aad051adbc8acc1e52caeb5f6
Max Sveshnikov <msveshnikov@thumbtack.net>
2014-09-23 23:46:31 +0700

    3 types of response

--- Rest/src/main/java/bench/UrlShortener.java
+++ Rest/src/main/java/bench/UrlShortener.java
@@ -1,13 +1,20 @@
 package main.java.bench;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
 @Path("/")
 public class UrlShortener {
 
     @GET
     @Produces("text/plain")
     @Path("{shorturl}")
-    public String redirect(@PathParam("shorturl") String shortUrl,@QueryParam("url") String longUrl) {
-        return "Redirect to " + shortUrl+" long="+longUrl;
+    public Response redirect(@PathParam("shorturl") String shortUrl, @QueryParam("url") String longUrl) {
+        if (shortUrl.equals("shorten")) {
+            if (longUrl != null)
+                return Response.status(Response.Status.OK).entity("Shortening long=" + longUrl).build();
+            else return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        } else
+            return Response.status(Response.Status.MOVED_PERMANENTLY).entity("Redirect to " + shortUrl).build();
     }
 }
